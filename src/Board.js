dabd97e37ea6cccc5565ccd38e97ac4e73fcefbb
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-02-23 15:41:53 -0800

    done

--- src/Board.js
+++ src/Board.js
@@ -34,7 +34,7 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   function createBoard() {
     
     let initialBoard = [];
-    // TODO: create array-of-arrays of true/false values : done
+   
     for (let x=0; x<nrows; x++) {
       const row = []
       for (let y=0; y<ncols; y++) {
@@ -46,7 +46,7 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   }
 
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
+    return board.every(row => row.every(cell => !cell));
   }
 
   function flipCellsAround(coord) {
@@ -71,19 +71,13 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
 
       return boardCopy;
 
-      // TODO: Make a (deep) copy of the oldBoard
-
-      // TODO: in the copy, flip this cell and the cells around it
-
-      // TODO: return the copy
     });
   }
 
-  // if the game is won, just show a winning msg & render nothing else
+  if (hasWon()) {
+    return <div>You Win!</div>;
+  }
 
-  // TODO
-
-  // make table board
   let tableBoard = []
   for(let x=0; x<nrows; x++){
     let row = []
@@ -108,8 +102,6 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
     </table>
   );
 
-
-  // TODO
 }
 
 export default Board;
